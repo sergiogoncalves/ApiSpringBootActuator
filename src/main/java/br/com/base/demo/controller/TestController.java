4e6c038cd88fde41d864914307cc67d4358d9f0f
Sérgio Gonçalves <eu.sergiogoncalves@gmail.com>
2019-03-28 10:43:02 -0400

    Spring boot actuator log

--- src/main/java/br/com/base/demo/controller/TestController.java
+++ src/main/java/br/com/base/demo/controller/TestController.java
@@ -1,5 +1,7 @@
 package br.com.base.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.metrics.MetricsEndpoint;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,11 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     private MetricsEndpoint counterService;
+    private static Logger log = LoggerFactory.getLogger(TestController.class);
 
 
     @RequestMapping(path = "/", produces = "application/json", method = RequestMethod.GET)
     private String getStandard() {
 
+        log.error("Testando erro de log");
 
         return "Standard";
 
